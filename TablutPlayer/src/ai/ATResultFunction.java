9adf19d1a1fa69d71980320e3e9a54d2eeb887ed
bonacciog <giovanni.bonaccio96@gmail.com>
2019-05-07 15:53:16 +0200

    Aggiunte una classe per generazione azioni

---
+++ TablutPlayer/src/ai/ATResultFunction.java
@@ -0,0 +1,14 @@
+package ai;
+
+import aima.core.agent.Action;
+import aima.core.search.framework.problem.ResultFunction;
+
+public class ATResultFunction implements ResultFunction{
+
+	@Override
+	public Object result(Object arg0, Action arg1) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
